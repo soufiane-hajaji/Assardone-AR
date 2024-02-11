d246082e02a01a97798da99b5618c3ab319e3da4
Soufiane Hajaji <86585631+soufiane-hajaji@users.noreply.github.com>
2024-02-11 15:09:05 +0100

    Update FontAwesome.js

--- FontAwesome.js
+++ FontAwesome.js
@@ -1,18 +1,7 @@
-<script>//<![CDATA[
-var domain = window.location.hostname;
-var passedDomain = domain.split(".").slice(-2).join(".");
-
-
-var actualDomain = "bzoukasft.com"; 
-
-
-if (passedDomain !== actualDomain) {
-
-    document.documentElement.innerHTML = '';
-
-    var message = document.createElement('div');
-    message.innerHTML = 'عذرًا، هذا الدومين غير مصرح به.';
-    document.body.appendChild(message);
-
+body * {
+    display: none !important;
 }
-//]]></script>
+
+body {
+    background-color: #ffffff !important;
+}
